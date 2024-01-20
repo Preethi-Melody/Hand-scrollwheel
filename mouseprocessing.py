4f5c14e10c2719d00968ff9525357e4ce177308f
Preethi-Melody <75930674+Preethi-Melody@users.noreply.github.com>
2024-01-20 14:34:33 +0530

    initial commit

---
+++ mouseprocessing.py
@@ -0,0 +1,10 @@
+import mediapipe as mp
+
+inactive = False
+active = True
+
+# def mouse_activity(hand_coordinates):
+#     if not Active(hand_coordinates):
+#         prev = inactive
+#     else:
+#         if prev
